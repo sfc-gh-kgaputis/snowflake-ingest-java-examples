e43015edd30d0923edb9e95275b4c42485dd047d
Keith Gaputis <keith.gaputis@snowflake.com>
2023-05-10 10:21:13 -0400

    Update basic example to use environment variables instead of hard-coded connection info, enable SSL

--- src/main/java/net/snowflake/ingest/example/IngestExampleHelper.java
+++ src/main/java/net/snowflake/ingest/example/IngestExampleHelper.java
@@ -33,130 +33,131 @@ import org.slf4j.LoggerFactory;
 
 /** Helper methods to connect to snowflake jdbc and run queries Created by vganesh on 5/19/17. */
 public class IngestExampleHelper {
-  // a logger for all of our needs in this class
-  private static final Logger LOGGER = LoggerFactory.getLogger(IngestExampleHelper.class.getName());
-  /**
-   * Create directories if they don't exist under directoryPath.
-   *
-   * @param directoryPath
-   * @throws IOException
-   */
-  public static void makeLocalDirectory(String directoryPath) throws IOException {
-    Files.createDirectories(Paths.get(directoryPath));
-  }
-
-  public static Path createTempCsv(String directoryPath, String filename, int rows)
-      throws IOException {
-    final ThreadLocalRandom rnd = ThreadLocalRandom.current();
-
-    final Path csv = Files.createTempFile(Paths.get(directoryPath), filename, ".csv");
-    final String fileName = csv.getFileName().toString();
-
-    try (Writer w = Files.newBufferedWriter(csv, StandardCharsets.UTF_8)) {
-      for (int i = 0; i < rows; i++) {
-        w.write(String.valueOf(i));
-        w.write(",");
-        w.write("row-");
-        w.write(String.valueOf(i));
-        w.write(",");
-        w.write(String.valueOf(rnd.nextLong()));
-        w.write(",");
-        w.write(fileName);
-        w.write("\n");
-      }
-    }
-    return csv;
-  }
-
-  /**
-   * Create file under directoryPath with fileName and populate it with test data
-   *
-   * @param directoryPath
-   * @param filename
-   * @throws IOException
-   */
-  public static void makeSampleFile(String directoryPath, String filename) throws IOException {
-
-    File file = new File(directoryPath, filename);
-
-    // if our file doesn't already exist
-    if (!file.exists()) {
-      // create it
-      file.createNewFile();
-
-      // populate it with some data
-      FileWriter fw = new FileWriter(file.getAbsoluteFile());
-      BufferedWriter bw = new BufferedWriter(fw);
-      for (char letter = 'a'; letter <= 'z'; letter++) {
-        bw.write(letter + "\n");
-      }
-      // close it back up
-      bw.close();
+    // a logger for all of our needs in this class
+    private static final Logger LOGGER = LoggerFactory.getLogger(IngestExampleHelper.class.getName());
+    /**
+     * Create directories if they don't exist under directoryPath.
+     *
+     * @param directoryPath
+     * @throws IOException
+     */
+    public static void makeLocalDirectory(String directoryPath) throws IOException {
+        Files.createDirectories(Paths.get(directoryPath));
     }
 
-    return;
-  }
+    public static Path createTempCsv(String directoryPath, String filename, int rows)
+            throws IOException {
+        final ThreadLocalRandom rnd = ThreadLocalRandom.current();
 
-  public static Connection getConnection(
-      String user, String password, String account, String host, int port) throws Exception {
-    // check first to see if we have the Snowflake JDBC
-    Class.forName("net.snowflake.client.jdbc.SnowflakeDriver");
+        final Path csv = Files.createTempFile(Paths.get(directoryPath), filename, ".csv");
+        final String fileName = csv.getFileName().toString();
 
-    // build our properties
-    Properties props = new Properties();
-    props.put("user", user);
-    props.put("password", password);
-    props.put("account", account);
-    props.put("ssl", "off");
-
-    // the actual connection string
-    String connectString = "jdbc:snowflake://" + host + ":" + port;
-
-    // fire off the connection
-    return DriverManager.getConnection(connectString, props);
-  }
-
-  /**
-   * Try to execute a SQL query and throw if it fails
-   *
-   * @param query the query in question
-   */
-  public static void doQuery(Connection conn, String query) {
-    LOGGER.info("doQuery {}", query);
-    try (Statement statement = conn.createStatement()) {
-      statement.executeQuery(query);
+        try (Writer w = Files.newBufferedWriter(csv, StandardCharsets.UTF_8)) {
+            for (int i = 0; i < rows; i++) {
+                w.write(String.valueOf(i));
+                w.write(",");
+                w.write("row-");
+                w.write(String.valueOf(i));
+                w.write(",");
+                w.write(String.valueOf(rnd.nextLong()));
+                w.write(",");
+                w.write(fileName);
+                w.write("\n");
+            }
+        }
+        return csv;
     }
-    // if ANY exceptions occur, an illegal state has been reached
-    catch (Exception e) {
-      throw new IllegalStateException(e);
+
+    /**
+     * Create file under directoryPath with fileName and populate it with test data
+     *
+     * @param directoryPath
+     * @param filename
+     * @throws IOException
+     */
+    public static void makeSampleFile(String directoryPath, String filename) throws IOException {
+
+        File file = new File(directoryPath, filename);
+
+        // if our file doesn't already exist
+        if (!file.exists()) {
+            // create it
+            file.createNewFile();
+
+            // populate it with some data
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            for (char letter = 'a'; letter <= 'z'; letter++) {
+                bw.write(letter + "\n");
+            }
+            // close it back up
+            bw.close();
+        }
+
+        return;
     }
-  }
 
-  private static String ALGORITHM = "RSA";
-  /**
-   * Generates an RSA keypair for use in this test
-   *
-   * @return a valid RSA keypair
-   * @throws NoSuchAlgorithmException if we don't have an RSA algo
-   * @throws NoSuchProviderException if we can't use SHA1PRNG for randomization
-   */
-  public static KeyPair generateKeyPair() throws NoSuchProviderException, NoSuchAlgorithmException {
-    KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITHM);
-    SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
-    keyGen.initialize(2048, random);
-    return keyGen.generateKeyPair();
-  }
+    public static Connection getConnection(
+            String user, String password, String account, String host, int port) throws Exception {
+        // check first to see if we have the Snowflake JDBC
+        Class.forName("net.snowflake.client.jdbc.SnowflakeDriver");
 
-  /**
-   * Generate the public key as a string
-   *
-   * @return the public key as a string
-   */
-  public static String getPublicKeyString(KeyPair keypair)
-      throws NoSuchAlgorithmException, InvalidKeySpecException {
-    KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM);
-    final PublicKey pk = keypair.getPublic();
-    X509EncodedKeySpec spec = keyFactory.getKeySpec(pk, X509EncodedKeySpec.class);
-    return Base64.encodeBase64String(spec.getEncoded());
-  }
+
+        // build our properties
+        Properties props = new Properties();
+        props.put("user", user);
+        props.put("password", password);
+        props.put("account", account);
+        props.put("ssl", "true");
+
+        // the actual connection string
+        String connectString = "jdbc:snowflake://" + host + ":" + port;
+
+        // fire off the connection
+        return DriverManager.getConnection(connectString, props);
+    }
+
+    /**
+     * Try to execute a SQL query and throw if it fails
+     *
+     * @param query the query in question
+     */
+    public static void doQuery(Connection conn, String query) {
+        LOGGER.info("doQuery {}", query);
+        try (Statement statement = conn.createStatement()) {
+            statement.executeQuery(query);
+        }
+        // if ANY exceptions occur, an illegal state has been reached
+        catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private static String ALGORITHM = "RSA";
+    /**
+     * Generates an RSA keypair for use in this test
+     *
+     * @return a valid RSA keypair
+     * @throws NoSuchAlgorithmException if we don't have an RSA algo
+     * @throws NoSuchProviderException if we can't use SHA1PRNG for randomization
+     */
+    public static KeyPair generateKeyPair() throws NoSuchProviderException, NoSuchAlgorithmException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITHM);
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        keyGen.initialize(2048, random);
+        return keyGen.generateKeyPair();
+    }
+
+    /**
+     * Generate the public key as a string
+     *
+     * @return the public key as a string
+     */
+    public static String getPublicKeyString(KeyPair keypair)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+        KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM);
+        final PublicKey pk = keypair.getPublic();
+        X509EncodedKeySpec spec = keyFactory.getKeySpec(pk, X509EncodedKeySpec.class);
+        return Base64.encodeBase64String(spec.getEncoded());
+    }
 }
--- src/main/java/net/snowflake/ingest/example/SnowflakeIngestBasicExample.java
+++ src/main/java/net/snowflake/ingest/example/SnowflakeIngestBasicExample.java
@@ -28,222 +28,228 @@ import net.snowflake.ingest.utils.StagedFileWrapper;
  * <p><b>Prerequisites:</b> You must have a valid set of Snowflake credentials
  */
 public class SnowflakeIngestBasicExample {
-  // Details required to connect to Snowflake over jdbc and establish
-  // pre-requesites
-  private static String account = "s3testaccount";
-  private static String user = "snowman";
-  private static String host = "s3testaccount.snowflakecomputing.com";
-  private static String scheme = "https";
-  private static String password = "****";
-  private static int port = 8080;
+    // Details required to connect to Snowflake over jdbc and establish
+    // pre-requesites
 
-  // Details for the pipe which we are going to use
-  // the name of our target DB
-  private static String database = "testdb";
-  // the name of our target schema
-  private static String schema = "public";
-  // the name of our stage
-  private static String stage = "ingest_stage";
-  // the name of our target table
-  private static String table = "ingest_table";
-  // the name of our pipe
-  private static String pipe = "ingest_pipe";
+    private static String account = System.getenv("SNOWFLAKE_ACCOUNT");
+    private static String user = System.getenv("SNOWFLAKE_USER");
+    private static String host = account + ".snowflakecomputing.com";
+    private static String scheme = "https";
+    private static String password = System.getenv("SNOWFLAKE_PASSWORD");
+    private static int port = 443;
 
-  // the connection we will use for queries
-  private static Connection conn;
+    // Details for the pipe which we are going to use
+    // the name of our target DB
+    private static String database = "testdb";
+    // the name of our target schema
+    private static String schema = "public";
+    // the name of our stage
+    private static String stage = "ingest_stage";
+    // the name of our target table
+    private static String table = "ingest_table";
+    // the name of our pipe
+    private static String pipe = "ingest_pipe";
 
-  // the Administrative connection
-  // the fully qualified pipe name
-  private static String fqPipe = database + "." + schema + "." + pipe;
+    // the connection we will use for queries
+    private static Connection conn;
 
-  // the actual ingest manager
-  private static SimpleIngestManager manager;
+    // the Administrative connection
+    // the fully qualified pipe name
+    private static String fqPipe = database + "." + schema + "." + pipe;
 
-  // our keypair
-  private static KeyPair keypair;
+    // the actual ingest manager
+    private static SimpleIngestManager manager;
 
-  /** Creates the stages and files we'll use for this test */
-  private static void setup(Set<String> files, String filesLocation) throws Exception {
-    // use the right database
-    IngestExampleHelper.doQuery(conn, "use database " + database);
+    // our keypair
+    private static KeyPair keypair;
 
-    // use the right schema
-    IngestExampleHelper.doQuery(conn, "use schema " + schema);
+    /**
+     * Creates the stages and files we'll use for this test
+     */
+    private static void setup(Set<String> files, String filesLocation) throws Exception {
+        // use the right database
+        IngestExampleHelper.doQuery(conn, "use database " + database);
 
-    // create the target stage
-    IngestExampleHelper.doQuery(
-        conn, "create or replace stage " + stage + " FILE_FORMAT=(type='csv' COMPRESSION=NONE)");
+        // use the right schema
+        IngestExampleHelper.doQuery(conn, "use schema " + schema);
 
-    // create the target
-    IngestExampleHelper.doQuery(
-        conn,
-        "create or replace table " + table + " (row_id int, row_str string, num int, src string)");
-    // Create the pipe for subsequently ingesting files to.
-    IngestExampleHelper.doQuery(
-        conn,
-        "create or replace pipe "
-            + pipe
-            + " as copy into "
-            + table
-            + " from @"
-            + stage
-            + " file_format=(type='csv')");
+        // create the target stage
+        IngestExampleHelper.doQuery(
+                conn, "create or replace stage " + stage + " FILE_FORMAT=(type='csv' COMPRESSION=NONE)");
 
-    String pk = IngestExampleHelper.getPublicKeyString(keypair);
+        // create the target
+        IngestExampleHelper.doQuery(
+                conn,
+                "create or replace table " + table + " (row_id int, row_str string, num int, src string)");
+        // Create the pipe for subsequently ingesting files to.
+        IngestExampleHelper.doQuery(
+                conn,
+                "create or replace pipe "
+                        + pipe
+                        + " as copy into "
+                        + table
+                        + " from @"
+                        + stage
+                        + " file_format=(type='csv')");
 
-    // assume the necessary privileges
-    IngestExampleHelper.doQuery(conn, "use role accountadmin");
+        String pk = IngestExampleHelper.getPublicKeyString(keypair);
 
-    // set the public key
-    IngestExampleHelper.doQuery(conn, "alter user " + user + " set RSA_PUBLIC_KEY='" + pk + "'");
-    files.forEach(
-        file -> {
-          IngestExampleHelper.doQuery(
-              conn, "PUT " + filesLocation + file + " @" + stage + " AUTO_COMPRESS=FALSE");
-        });
+        // assume the necessary privileges
+        IngestExampleHelper.doQuery(conn, "use role accountadmin");
 
-    IngestExampleHelper.doQuery(conn, "use role sysadmin");
-  }
+        // set the public key
+        IngestExampleHelper.doQuery(conn, "alter user " + user + " set RSA_PUBLIC_KEY='" + pk + "'");
+        files.forEach(
+                file -> {
+                    IngestExampleHelper.doQuery(
+                            conn, "PUT " + filesLocation + file + " @" + stage + " AUTO_COMPRESS=FALSE");
+                });
 
-  /**
-   * Ingest a single file from the staging are using ingest service
-   *
-   * @param filename
-   * @return
-   * @throws Exception
-   */
-  private static IngestResponse insertFile(String filename) throws Exception {
-    // create a file wrapper
-    StagedFileWrapper myFile = new StagedFileWrapper(filename, null);
-    return manager.ingestFile(myFile, null);
-  }
+        IngestExampleHelper.doQuery(conn, "use role sysadmin");
+    }
 
-  /**
-   * Ingest a set of files from the staging area through the ingest service
-   *
-   * @param files
-   * @return
-   * @throws Exception
-   */
-  private static IngestResponse insertFiles(Set<String> files) throws Exception {
-    return manager.ingestFiles(manager.wrapFilepaths(files), null);
-  }
+    /**
+     * Ingest a single file from the staging are using ingest service
+     *
+     * @param filename
+     * @return
+     * @throws Exception
+     */
+    private static IngestResponse insertFile(String filename) throws Exception {
+        // create a file wrapper
+        StagedFileWrapper myFile = new StagedFileWrapper(filename, null);
+        return manager.ingestFile(myFile, null);
+    }
 
-  /**
-   * Given a set of files, keep querying history until all of files that are in the set have been
-   * seen in the history. Times out after 2 minutes
-   *
-   * @param files
-   * @return HistoryResponse containing only the FileEntries corresponding to requested input
-   * @throws Exception
-   */
-  private static HistoryResponse waitForFilesHistory(Set<String> files) throws Exception {
-    ExecutorService service = Executors.newSingleThreadExecutor();
+    /**
+     * Ingest a set of files from the staging area through the ingest service
+     *
+     * @param files
+     * @return
+     * @throws Exception
+     */
+    private static IngestResponse insertFiles(Set<String> files) throws Exception {
+        return manager.ingestFiles(manager.wrapFilepaths(files), null);
+    }
 
-    class GetHistory implements Callable<HistoryResponse> {
-      private Set<String> filesWatchList;
+    /**
+     * Given a set of files, keep querying history until all of files that are in the set have been
+     * seen in the history. Times out after 2 minutes
+     *
+     * @param files
+     * @return HistoryResponse containing only the FileEntries corresponding to requested input
+     * @throws Exception
+     */
+    private static HistoryResponse waitForFilesHistory(Set<String> files) throws Exception {
+        ExecutorService service = Executors.newSingleThreadExecutor();
 
-      GetHistory(Set<String> files) {
-        this.filesWatchList = files;
-      }
+        class GetHistory implements Callable<HistoryResponse> {
+            private Set<String> filesWatchList;
 
-      String beginMark = null;
-
-      public HistoryResponse call() throws Exception {
-        HistoryResponse filesHistory = null;
-        while (true) {
-          Thread.sleep(500);
-          HistoryResponse response = manager.getHistory(null, null, beginMark);
-          if (response.getNextBeginMark() != null) {
-            beginMark = response.getNextBeginMark();
-          }
-          if (response != null && response.files != null) {
-            for (HistoryResponse.FileEntry entry : response.files) {
-              // if we have a complete file that we've
-              // loaded with the same name..
-              String filename = entry.getPath();
-              if (entry.getPath() != null
-                  && entry.isComplete()
-                  && filesWatchList.contains(filename)) {
-                if (filesHistory == null) {
-                  filesHistory = new HistoryResponse();
-                  filesHistory.setPipe(response.getPipe());
-                }
-                filesHistory.files.add(entry);
-                filesWatchList.remove(filename);
-                // we can return true!
-                if (filesWatchList.isEmpty()) {
-                  return filesHistory;
-                }
-              }
+            GetHistory(Set<String> files) {
+                this.filesWatchList = files;
             }
-          }
+
+            String beginMark = null;
+
+            public HistoryResponse call() throws Exception {
+                HistoryResponse filesHistory = null;
+                while (true) {
+                    Thread.sleep(500);
+                    HistoryResponse response = manager.getHistory(null, null, beginMark);
+                    if (response.getNextBeginMark() != null) {
+                        beginMark = response.getNextBeginMark();
+                    }
+                    if (response != null && response.files != null) {
+                        for (HistoryResponse.FileEntry entry : response.files) {
+                            // if we have a complete file that we've
+                            // loaded with the same name..
+                            String filename = entry.getPath();
+                            if (entry.getPath() != null
+                                    && entry.isComplete()
+                                    && filesWatchList.contains(filename)) {
+                                if (filesHistory == null) {
+                                    filesHistory = new HistoryResponse();
+                                    filesHistory.setPipe(response.getPipe());
+                                }
+                                filesHistory.files.add(entry);
+                                filesWatchList.remove(filename);
+                                // we can return true!
+                                if (filesWatchList.isEmpty()) {
+                                    return filesHistory;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
-      }
+
+        GetHistory historyCaller = new GetHistory(files);
+        // fork off waiting for a load to the service
+        Future<HistoryResponse> result = service.submit(historyCaller);
+
+        HistoryResponse response = result.get(2, TimeUnit.MINUTES);
+        return response;
     }
 
-    GetHistory historyCaller = new GetHistory(files);
-    // fork off waiting for a load to the service
-    Future<HistoryResponse> result = service.submit(historyCaller);
+    public static void main(String[] args) throws IOException {
+        final String filesDirectory = "/tmp/data/";
+        final String filesLocationUrl = "file:///tmp/data/";
+        // The first few steps simulate example files that we will write to the
+        // ingest service.
+        // base file name we want to load
+        final String fileWithWrongCSVFormat = "letters.csv";
+        IngestExampleHelper.makeLocalDirectory(filesDirectory);
+        IngestExampleHelper.makeSampleFile(filesDirectory, fileWithWrongCSVFormat);
+        Set<String> files = new TreeSet<>();
+        files.add(
+                IngestExampleHelper.createTempCsv(filesDirectory, "sample", 10).getFileName().toString());
+        files.add(
+                IngestExampleHelper.createTempCsv(filesDirectory, "sample", 15).getFileName().toString());
+        files.add(
+                IngestExampleHelper.createTempCsv(filesDirectory, "sample", 20).getFileName().toString());
+        System.out.println("Starting snowflake ingest client");
+        System.out.println(
+                "Connecting to "
+                        + scheme
+                        + "://"
+                        + host
+                        + ":"
+                        + port
+                        + "\n with Account:"
+                        + account
+                        + ", User: "
+                        + user);
 
-    HistoryResponse response = result.get(2, TimeUnit.MINUTES);
-    return response;
-  }
+        try {
+            final long oneHourMillis = 1000 * 3600l;
+            String startTime =
+                    Instant.ofEpochMilli(System.currentTimeMillis() - 4 * oneHourMillis).toString();
+            System.out.println("Getting connection...");
+            conn = IngestExampleHelper.getConnection(user, password, account, host, port);
+            System.out.println("Generating keypair...");
+            keypair = IngestExampleHelper.generateKeyPair();
+            System.out.println("Starting ingest process...");
+            manager = new SimpleIngestManager(account, user, fqPipe, keypair, scheme, host, port);
+            files.add(fileWithWrongCSVFormat);
+            setup(files, filesLocationUrl);
+            IngestResponse response = insertFiles(files);
+            System.out.println("Received ingest response: " + response.toString());
+            response = insertFile(fileWithWrongCSVFormat);
+            System.out.println("Received ingest response: " + response.toString());
+            // This will show up as a file that was not loaded in history
+            HistoryResponse history = waitForFilesHistory(files);
+            System.out.println("Received history response: " + history.toString());
+            String endTime = Instant.ofEpochMilli(System.currentTimeMillis()).toString();
 
-  public static void main(String[] args) throws IOException {
-    final String filesDirectory = "/tmp/data/";
-    final String filesLocationUrl = "file:///tmp/data/";
-    // The first few steps simulate example files that we will write to the
-    // ingest service.
-    // base file name we want to load
-    final String fileWithWrongCSVFormat = "letters.csv";
-    IngestExampleHelper.makeLocalDirectory(filesDirectory);
-    IngestExampleHelper.makeSampleFile(filesDirectory, fileWithWrongCSVFormat);
-    Set<String> files = new TreeSet<>();
-    files.add(
-        IngestExampleHelper.createTempCsv(filesDirectory, "sample", 10).getFileName().toString());
-    files.add(
-        IngestExampleHelper.createTempCsv(filesDirectory, "sample", 15).getFileName().toString());
-    files.add(
-        IngestExampleHelper.createTempCsv(filesDirectory, "sample", 20).getFileName().toString());
-    System.out.println("Starting snowflake ingest client");
-    System.out.println(
-        "Connecting to "
-            + scheme
-            + "://"
-            + host
-            + ":"
-            + port
-            + "\n with Account:"
-            + account
-            + ", User: "
-            + user);
+            HistoryRangeResponse historyRangeResponse = manager.getHistoryRange(null, startTime, endTime);
 
-    try {
-      final long oneHourMillis = 1000 * 3600l;
-      String startTime =
-          Instant.ofEpochMilli(System.currentTimeMillis() - 4 * oneHourMillis).toString();
-      conn = IngestExampleHelper.getConnection(user, password, account, host, port);
-      keypair = IngestExampleHelper.generateKeyPair();
-      manager = new SimpleIngestManager(account, user, fqPipe, keypair, scheme, host, port);
-      files.add(fileWithWrongCSVFormat);
-      setup(files, filesLocationUrl);
-      IngestResponse response = insertFiles(files);
-      System.out.println("Received ingest response: " + response.toString());
-      response = insertFile(fileWithWrongCSVFormat);
-      System.out.println("Received ingest response: " + response.toString());
-      // This will show up as a file that was not loaded in history
-      HistoryResponse history = waitForFilesHistory(files);
-      System.out.println("Received history response: " + history.toString());
-      String endTime = Instant.ofEpochMilli(System.currentTimeMillis()).toString();
-
-      HistoryRangeResponse historyRangeResponse = manager.getHistoryRange(null, startTime, endTime);
-
-      System.out.println("Received history range response: " + historyRangeResponse.toString());
-    } catch (IngestResponseException e) {
-      System.out.println("Service exception: " + e.toString());
-    } catch (Exception e) {
-      System.out.println(e);
+            System.out.println("Received history range response: " + historyRangeResponse.toString());
+        } catch (IngestResponseException e) {
+            System.out.println("Service exception: " + e.toString());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
-  }
 }
